06fc182034e4c37299d25fac3631d71fe0d1a90a
Shellpie <2827144556@qq.com>
2016-04-04 20:08:11 +0800

    Update YSFJ.java

--- YSFJ.java
+++ YSFJ.java
@@ -9,9 +9,8 @@ class YSFJ {
 	}
 	
 	public void getYSFJ () {
-		System.out.println("0");
 		if (a == 1) {
-		 simpleYSFJ();	
+			simpleYSFJ();	
 		}else{
 			defaultYSFJ();	
 		}
@@ -19,28 +18,15 @@ class YSFJ {
 	}
 	
 	private int simpleYSFJ(){
-		System.out.println("1");
 		for (int i = 1; i < c/2+1; i++){
-			System.out.println("2");
 			double j = c/i;
 			System.out.println(i + " "  + j);
 			if (c/i + i == b){
 				a1 = a2 = 1; 
 				b1 = i;	
 				b2 = c/i;
-				return 0;
 			}
 		}
-			for (int i = -c; i < 0; i++){
-				double j = c/i;
-				System.out.println(i + " "  + j);
-				if (j + i == b && c % i == 0 && i * j == c){
-				a1 = a2 = 1;
-				b1 = i;	
-				b2 = c/i;
-				return 0;
-				}
-			}
 		return 0;
 	}
 	
